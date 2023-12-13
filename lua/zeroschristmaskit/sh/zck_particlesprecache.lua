788cd1987bcbe79a353aa057b7bfb24037ee46f3
Sandalot <20023123+Sandalot@users.noreply.github.com>
2023-12-13 17:52:19 +1100

    Initial commit
    Addon from Steam workshop
    Workshop ID: 1596556401
    13/Nov/23

---
+++ lua/zeroschristmaskit/sh/zck_particlesprecache.lua
@@ -0,0 +1,6 @@
+AddCSLuaFile()
+
+game.AddParticles("particles/zck_snowball.pcf")
+PrecacheParticleSystem( "zck_snowball_explode" )
+PrecacheParticleSystem( "zck_snowball_pickup" )
+PrecacheParticleSystem( "zck_snowball_trail" )
